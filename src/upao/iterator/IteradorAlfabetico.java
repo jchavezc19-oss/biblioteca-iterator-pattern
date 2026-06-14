11691026af79290e2a43c44db70ba9d548079e8f
Leonardo Chavez <jchavezc19@upao.edu.pe>
2026-06-13 19:16:12 -0500

    feat: definir interfaces del patron iterator y entidad core del sistema

---
+++ src/upao/iterator/IteradorAlfabetico.java
@@ -0,0 +1,38 @@
+package upao.iterator;
+import upao.model.RecursoBibliografico;     
+    
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Código base desarrollado por: Chávez Leonardo
+ * Propósito: Recorrido secuencial ordenado alfabéticamente por título.
+ */
+public class IteradorAlfabetico implements IteradorInterface {
+    private List<RecursoBibliografico> listaOrdenada;
+    private int indice = 0; // Puntero de posición actual
+
+    public IteradorAlfabetico(List<RecursoBibliografico> recursosOriginales) {
+        // Clonación defensiva para aislar los datos internos y no alterar la base de datos original
+        this.listaOrdenada = new ArrayList<>(recursosOriginales);
+        
+        // Algoritmo de ordenamiento secuencial por título de la A a la Z
+        this.listaOrdenada.sort(Comparator.comparing(r -> r.getTitulo().toLowerCase()));
+    }
+
+    @Override
+    public boolean hasNext() {
+        // Retorna verdadero si el puntero no ha superado el tamaño máximo de la lista ordenada
+        return indice < listaOrdenada.size();
+    }
+
+    @Override
+    public RecursoBibliografico next() {
+        if (!hasNext()) {
+            return null;
+        }
+        // Extrae el recurso en la posición del puntero y lo incrementa de inmediato (++ en postfijo)
+        return listaOrdenada.get(indice++);
+    }
+}
